20bfa171294509b013b98816e49c8c552cb11e5a
IancuMarius99 <b.iancumarius@gmail.com>
2022-08-25 20:53:38 +0300

    1

--- FINAL-PROJECT/Reels.js
+++ FINAL-PROJECT/Reels.js
@@ -5,12 +5,16 @@ let q4 = 'Lorem Ipsum4';
 let q5 = 'Lorem Ipsum5';
 const quotes = [q1, q2, q3, q4, q5];
 
-function quotesFunction () {
-    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; 
-    document.getElementById('quote-display').innerHTML= randomQuote;  
-    }; 
-    
+function quotesFunction() {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    document.getElementById('quote-display').innerHTML = randomQuote;
+};
 
-const films = [
-    
-]
+let films = [];
+films.push('https://www.google.com/search?q=homeland+poster&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjAwrKAsuL5AhVt57sIHRB2CNoQ_AUoAXoECAEQDg&biw=1920&bih=961#imgrc=tyNcaGdLp6UqpM');
+films.push('https://www.google.com/search?q=the+office+poster&oq=the+office+poster&aqs=chrome.0.0i512l8j46i512j0i512.7989j0j9&sourceid=chrome&ie=UTF-8#imgrc=962xdaJOkM51WM');
+
+function filmsFunction() {
+    const randomFilm = films[Math.floor(Math.random() * films.length)];
+    document.getElementById('film-display').innerHTML = randomFilm;
+}; 
