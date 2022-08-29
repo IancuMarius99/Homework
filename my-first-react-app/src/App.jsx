335bfe99ab489da1c56db008b87b1b436a2dc657
IancuMarius99 <b.iancumarius@gmail.com>
2022-08-29 18:01:47 +0300

    1

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import Layout from '../components/Layout/Layout'
 import HomePage from '../pages/HomePage/HomePage'
+import { CartPage } from '../pages/CartPage/CartPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import cartCounter from '../pages/HomePage/HomePage';
+
 
 function App() {
-
+ // aici tinem statul cu itemsCount
   return (
-    <Layout>
-      <HomePage />
-    </Layout>
+    <BrowserRouter>
+      <Layout itemsCount={33}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="*" element={<div>Not Found!</div>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
-export default App;
+
+export default App
