2d88cc72ff9517f186769e66026bc7624d3015e2
IancuMarius99 <b.iancumarius@gmail.com>
2022-08-30 17:25:28 +0300

    1

--- FINAL-PROJECT/Reels.js
+++ FINAL-PROJECT/Reels.js
@@ -1,18 +1,21 @@
-let q1 = 'Lorem Ipsum1';
-let q2 = 'Lorem Ipsum2';
-let q3 = 'Lorem Ipsum3';
-let q4 = 'Lorem Ipsum4';
-let q5 = 'Lorem Ipsum5';
+let q1 = 'Never give in, never give in, never, never, never, never—in nothing, great or small, large or petty—never give in except to convictions of honour and good sense. - by Sir. Winston Churchill';
+let q2 = 'If you want to be happy, be... - by Lev Tolstoi';
+let q3 = 'I love not Man the less, but Nature more - by Lord Byron';
+let q4 = 'One of the penalties for refusing to participate in politics is that you end up being governed by your inferiors. - by Plato';
+let q5 = "'If voting changed anything, they'd make it illegal. - by Emma Goldman";
 const quotes = [q1, q2, q3, q4, q5];
+
+let f1 = 'The Office US';
+let f2 = 'Brookyn 99';
+let f3 = 'Homeland';
+let f4 = 'The Crown';
+let f5 = 'Shetland';
+const films = [f1, f2, f3, f4, f5];
 
 function quotesFunction() {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     document.getElementById('quote-display').innerHTML = randomQuote;
 };
-
-let films = [];
-films.push('https://www.google.com/search?q=homeland+poster&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjAwrKAsuL5AhVt57sIHRB2CNoQ_AUoAXoECAEQDg&biw=1920&bih=961#imgrc=tyNcaGdLp6UqpM');
-films.push('https://www.google.com/search?q=the+office+poster&oq=the+office+poster&aqs=chrome.0.0i512l8j46i512j0i512.7989j0j9&sourceid=chrome&ie=UTF-8#imgrc=962xdaJOkM51WM');
 
 function filmsFunction() {
     const randomFilm = films[Math.floor(Math.random() * films.length)];
